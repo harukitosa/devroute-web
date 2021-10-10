ff651fef09f88a6931e2779e48ba678f6774df62
harukitosa <13haruki28@gmail.com>
2021-10-10 19:57:32 +0900

    login

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,8 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import { useUser, login, logout } from "../src/auth";
+
 
 const Home: NextPage = () => {
   return (
@@ -52,6 +54,7 @@ const Home: NextPage = () => {
           </a>
         </div>
       </main>
+      <HomeComponent/>
 
       <footer className={styles.footer}>
         <a
@@ -68,5 +71,36 @@ const Home: NextPage = () => {
     </div>
   )
 }
+
+const HomeComponent: NextPage = () => {
+  const user = useUser();
+
+  const handleLogin = (): void => {
+    login().catch((error) => console.error(error));
+  };
+
+  const handleLogout = (): void => {
+    logout().catch((error) => console.error(error));
+  };
+
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Auth Example</title>
+      </Head>
+
+      <div>
+        <h1>Auth Example</h1>
+        {user !== null ? (
+          <h2>ログインしている</h2>
+        ) : (
+          <h2>ログインしていない</h2>
+        )}
+        <button onClick={handleLogin}>ログイン</button>
+        <button onClick={handleLogout}>ログアウト</button>
+      </div>
+    </div>
+  );
+};
 
 export default Home
